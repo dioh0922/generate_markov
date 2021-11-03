71a1723ce870d1bf55f81f5572232408d1cfcaee
dioh0922 <diohound@gmail.com>
2021-11-03 10:28:56 +0900

    heroku に転送する

--- generate_sentence_api.php
+++ generate_sentence_api.php
@@ -1,18 +1,28 @@
 <?php
+require_once(dirname(__FILE__)."/../env/url_setting.php");
+ini_set( 'display_errors', 1 );
 	header('Content-Type: text/html; charset=UTF-8');
-	$full = "";
-	if(isset($_POST["lang"])){
-		if($_POST["lang"] != ""){
-			if($_POST["lang"] == "jpn"){
-				$full = "python generate_sentence.py ./sonshi_jpn_pick.txt";
-			}else if($_POST["lang"] == "chn"){
-				$full = "python generate_sentence.py ./sonshi_pick.txt";
-			}
-			exec($full, $out);
+	try{
+		$full = "";
+		if(isset($_POST["lang"])){
+			$post_data = $_POST;
+			$curl = curl_init($HEROKU_MARKOV_URL);
+			curl_setopt($curl, CURLOPT_POST, TRUE);
+			curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post_data));
+			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+			curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);  //
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+			curl_setopt($curl, CURLOPT_COOKIEJAR,      'cookie');
+			curl_setopt($curl, CURLOPT_COOKIEFILE,     'tmp');
+			curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE); // Locationヘッダを追跡
 
-			foreach($out as $iter){
-				echo $iter;
-			}
+			$output = curl_exec($curl);
+			curl_close($curl);
+			echo $output;
+		}else{
+			echo "missing argument";
 		}
+	}catch(Exception $e){
+		echo $e->getMessage();
 	}
 ?>
